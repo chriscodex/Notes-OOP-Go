69f7cd5d4ec9d554251d5616a498c64340ec56db
Christian <3003html@gmail.com>
2022-06-15 18:43:24 -0500

    Implementing getters and setters

--- main.go
+++ main.go
@@ -2,10 +2,27 @@ package main
 
 import "fmt"
 
-// Structs (Equivalent to class)
+// Struct (Equivalent to classes)
 type Employee struct {
 	id   int
 	name string
+}
+
+// Receiver Function (Equivalent to methods)
+// Setters
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+// Getters
+func (e *Employee) GetId() int {
+	return e.id
+}
+func (e *Employee) GetName() string {
+	return e.name
 }
 
 func main() {
