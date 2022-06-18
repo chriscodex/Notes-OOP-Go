fd6ded205ae068e4d531eff0531e6b0fe6c01dcb
Christian <3003html@gmail.com>
2022-06-17 23:37:48 -0500

    Struct SMSNotificationSender created

--- Abstract Factory/abstractFactory.go
+++ Abstract Factory/abstractFactory.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Factory Notification
 type IntNotificationFactory interface {
 	SendNotification()
@@ -10,4 +12,25 @@ type IntNotificationFactory interface {
 type IntSender interface {
 	GetSenderMethod() string
 	GetSenderChannel() string
+}
+
+// Notifications
+type SMSNotification struct {
+}
+
+// Methods of SMSNotification
+func (SMSNotification) SendNotification() {
+	fmt.Println("Sending notification SMS")
+}
+
+// SMSNotificationSender
+type SMSNotificationSender struct {
+}
+
+// Methods of SMSNotificationSender
+func (SMSNotificationSender) GetSenderMethod() string {
+	return "SMS"
+}
+func (SMSNotificationSender) GetSenderChannel() string {
+	return "Twilio"
 }
